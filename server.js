b67803f5dd7d77ace04d5f293b933ce1fa808b41
spikeburton <ralphburton3@gmail.com>
2019-08-28 16:42:15 -0400

    move mock data and generator to db directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const JSONServer = require('json-server');
 
 const server = JSONServer.create();
-const router = JSONServer.router('db.json');
+const router = JSONServer.router('db/db.json');
 const middlewares = JSONServer.defaults();
 const port = process.env.PORT || 4000;
 
