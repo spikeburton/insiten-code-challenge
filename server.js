f62615b0102e5a5d8d7fb0aec47e659cd24f51d3
spikeburton <ralphburton3@gmail.com>
2019-08-28 12:06:33 -0400

    add mock data server

---
+++ server.js
@@ -0,0 +1,11 @@
+const JSONServer = require('json-server');
+
+const server = JSONServer.create();
+const router = JSONServer.router('db.json');
+const middlewares = JSONServer.defaults();
+const port = process.env.PORT || 4000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
